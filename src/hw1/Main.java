fedcd054d31e46ee9a41a11cfbc3631dc19c0d7b
Petr <monastyrevpetr@gmail.com>
2020-11-15 19:02:50 +0300

    hw1

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -2,6 +2,39 @@ package hw1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word!");
+        byte byteValue=-120;
+        short shortValue=1000;
+        int intValue=12442;
+        long longValue=2000000L;
+        float floatValue=12.34f;
+        double doubleValue=123.3;
+        char charValue='t';
+        boolean booleanValue=false;
+    }
+    public static float expression(float a, float b, float c, float d){
+        return a*(b+(c/d));
+    }
+    public static boolean interval(int a, int b){
+        return  (a+b)>=10 && (a+b)<=20;
+    }
+    public static void positiveNumber(int number){
+        if (number>=0){
+            System.out.println("Положительное");
+        }else {
+            System.out.println("Отрицательное");
+        }
+    }
+    public static boolean negativeNumber(int number){
+        return number<0;
+    }
+    public static void helloName(String name){
+        System.out.println("Привет, "+name+"!");
+    }
+    public static void leapYear(int year){
+        if ((year%400==0) || (year%4==0 && year%100!=0)){
+            System.out.println("Високосный");
+        }else{
+            System.out.println("Невисокосный");
+        }
     }
 }
